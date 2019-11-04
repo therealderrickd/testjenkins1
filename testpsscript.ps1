8a4d51ffb22e86ac151bb44a0483f224af562b6a
therealderrickd <dd+github@derrickdavis.net>
2019-11-04 15:57:01 -0500

    Update testpsscript.ps1

--- testpsscript.ps1
+++ testpsscript.ps1
@@ -1,3 +1,7 @@
+start-transcript C:\temp\transcript -force
+
 Get-LocalUser | out-file C:\temp\export_example_testjenkins.txt
 $PSVersionTable
 Write-Host "Hello World, I'm a Jenkins build!"
+
+Stop-Transcript
